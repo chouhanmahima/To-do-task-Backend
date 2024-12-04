77af0b3b1880f5f86eec6a1b0f2e03baa3f9cfb6
Mahima Chouhan <mahimachouhanmahi@gmail.com>
2024-12-04 21:51:14 +0530

    add cors

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ const app = express();
 dotenv.config();
 
 // Enable CORS for all routes
-app.use(cors({
-   origin: process.env.FRONTEND_URL, // Allow all origins, or specify a list of allowed origins  http://127.0.0.1:5500
-   methods: ['GET', 'POST'], // Allowed methods
-   credentials: true, // Allowed headers
-}));
+// app.use(cors({
+//    origin: process.env.FRONTEND_URL, // Allow all origins, or specify a list of allowed origins  http://127.0.0.1:5500
+// }));
+
+app.use(cors({ origin: "http://127.0.0.1:5500" }));
+
 
 // Middleware
 app.use(express.json());
